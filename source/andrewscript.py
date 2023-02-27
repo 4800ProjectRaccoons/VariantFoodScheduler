756f0deeef896010b9bf2ce756f9096b63d5fc3e
18anguyen24 <andrew.nguyen424@gmail.com>
2023-02-27 02:10:37 -0800

    add spoonacular api call and test

---
+++ source/andrewscript.py
@@ -0,0 +1,42 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+import random
+import requests
+
+spoonacular_api_key='1860d9cb5fb5432d894efb7ec63f484b'
+
+#get random food
+urlFood = "https://api.spoonacular.com/recipes/findByNutrients"
+max_calories = 1000
+query_params="apiKey=" + spoonacular_api_key + "&maxCalories=" + str(max_calories) + "&minCalories=" + str(max_calories - 100)
+query = urlFood + "?" + query_params
+spoonacular_response = requests.get(query)
+
+#store any random food from the query into variable
+datastore = random.choice(spoonacular_response.json())
+id = str(datastore['id'])
+
+#now get ingredients  using the id of the food
+urlFoodIngredients = "https://api.spoonacular.com/recipes/" + id + "/ingredientWidget.json"
+query_params_ingredients = "apiKey=" + spoonacular_api_key
+query_ingredients = urlFoodIngredients + "?" + query_params_ingredients
+#spoonacular_response = requests.get(query_ingredients)
+#ingredient_information = spoonacular_response.json()
+
+print(id + " /" + datastore['title'] + "/ " +  datastore['image'] + " Calories: " + str(datastore['calories']))
+#print(ingredient_information)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return "Hello world"
+
+@app.route("/result")
+def result():
+    food = id + " /" + datastore['title'] + "/ " +  datastore['image'] + " Calories: " + str(datastore['calories'])
+    return food
+
+if __name__ == "__main__":
+    app.run(debug= True, port=5001)
