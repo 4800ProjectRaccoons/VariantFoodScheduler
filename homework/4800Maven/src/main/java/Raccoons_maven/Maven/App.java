535e81d8534510ebd0209eb1d19062d5048f57f9
hnguyen07 <54465701+hnguyen07@users.noreply.github.com>
2023-03-01 19:21:15 -0800

    Reup Maven

---
+++ homework/4800Maven/src/main/java/Raccoons_maven/Maven/App.java
@@ -0,0 +1,12 @@
+package Raccoons_maven.Maven;
+
+public class App {
+
+	/**
+	 * Display Hello Raccoons
+ 	 */
+	public static void main( String[] args ) {
+    	System.out.println("Hello Raccoons!");
+    	
+    }
+}
---
+++ homework/4800Maven/src/test/java/Raccoons_maven/Maven/AppTest.java
@@ -0,0 +1,20 @@
+package Raccoons_maven.Maven;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
