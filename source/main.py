bc638c89c5050eaf2fdefce4901431401a8633c3
hnguyen07 <54465701+hnguyen07@users.noreply.github.com>
2023-03-13 23:33:16 -0700

    Add setUp and tearDown methods along with 2 more test cases

--- source/main.py
+++ source/main.py
@@ -2,17 +2,28 @@ import unittest
 from script import *
 
 class FoodTest(unittest.TestCase):
+    def setUp(self):
+        print('Preparing for testing...')
+        self.foo = FoodRecipes()
+
+    def tearDown(self):
+        print('Finishing and cleaning up the test...\n')
 
     def test1(self):
-        print("Print test 1...")
-        self.foo = FoodRecipes()
+        print("Beginning test 1...")
         self.assertEqual(self.foo.setCalorieRange(), 300)
 
     def test2(self):
-        print("Printing test 2...")
-        self.foo = FoodRecipes()
+        print("Beginning test 2...")
         self.assertEqual(self.foo.setCalorieRange(), 900)
 
+    def test3(self):
+        print("Beginning test 3...")
+        self.assertNotEqual(self.foo.setCalorieRange(), 850)
+
+    def test4(self):
+        print("Beginning test 4...")
+        self.assertNotEqual(self.foo.setCalorieRange(), 950)
 
 if __name__ == "__main__":
     unittest.main()
