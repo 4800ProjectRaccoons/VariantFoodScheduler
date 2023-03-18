098618e8fd9e9e25a8e32a63a93b6bd25dfd7ebe
fidelisprasetyo <fidelisprasetyo@gmail.com>
2023-03-18 14:19:32 -0700

    wired the spoonacular API to HTML page with HTTP API

--- src/main/source/main.py
+++ src/main/source/main.py
@@ -1,4 +1,17 @@
 from script import *
-    
-#if __name__ == "__main__":
-    
+from flask import Flask
+import json
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/randomize/<calories>")
+def randomize(calories):
+  calories = float(calories)
+  food = FoodRecipes(calories)
+  res = []
+  res.append(food.datastore)
+  return json.dumps(res)
+
+app.run(host = "0.0.0.0")
---
+++ src/main/source/script.js
@@ -0,0 +1,34 @@
+function getFood() {
+    console.log("Searching for food");
+    // grab input value
+    var input = $('#cal_input').val();
+    console.log("cal: " + input);
+  
+    // call the search server and get the result
+    $.ajax({
+      url: "http://127.0.0.1:5000/randomize/" + input,
+      success: function(res) {
+        console.log(res);
+  
+        $("#result_table").empty();
+        $("#result_table").append("<tr> <th>Name</th><th>Calories</th></tr>");
+        
+        console.log("parsing...")
+        var food = JSON.parse(res);
+
+        var food_title = food[0]["title"]
+        var food_calories = food[0]["calories"]
+
+        console.log(food_title);
+        console.log(food_calories);
+
+        var row_html_str = "<tr><td>" + food_title + "</td><td>" + food_calories + "</td></tr>"
+        $('#result_table').append(row_html_str);
+        // render the result in the list
+  
+      },
+      error: function(error) {
+        console.log(error.Message);
+      }
+    });
+  }
