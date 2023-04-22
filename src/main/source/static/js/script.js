2350b2ec3fc4e7c0d18df422d0fa6ed8a6336910
fidelisprasetyo <fidelisprasetyo@gmail.com>
2023-04-21 23:27:33 -0700

    add food image

--- src/main/source/static/js/script.js
+++ src/main/source/static/js/script.js
@@ -7,8 +7,8 @@ function getFood() {
   
     // call the search server and get the result
     $.ajax({
-      //url: "http://127.0.0.1:8080/randomize/" + input,
-      url: "http://ec2-3-128-204-114.us-east-2.compute.amazonaws.com:8080/randomize/" + input,
+      url: "http://127.0.0.1:8080/randomize/" + input,
+      //url: "http://ec2-3-128-204-114.us-east-2.compute.amazonaws.com:8080/randomize/" + input,
       success: function(res) {
         console.log(res);
   
@@ -21,11 +21,18 @@ function getFood() {
         var food_title = food[0]["title"]
         var food_calories = food[0]["calories"]
 
+        // image link
+        var food_img = "https://spoonacular.com/recipeImages/" + food[0]["id"] + "-" + "556x370" + ".jpg";
+
         console.log(food_title);
         console.log(food_calories);
+        console.log(food_img);
 
-        var row_html_str = "<tr><td>" + food_title + "</td><td>" + food_calories + "</td></tr>"
+        var row_html_str = "<tr><td>" + food_title + "</td><td>" + food_calories + "</td></tr>";
+        var row_html_img = "<tr><td><img src=" + food_img + "></td></tr>";
         $('#result_table').append(row_html_str);
+        $('#result_table').append(row_html_img);
+
         // render the result in the list
   
       },
