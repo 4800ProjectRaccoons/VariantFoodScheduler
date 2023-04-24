488b61efd956aea486b244fc894fdb027ea62530
fidelisprasetyo <fidelisprasetyo@gmail.com>
2023-04-23 17:46:44 -0700

    connected backend with the new frontend, added image

--- src/main/source/static/js/script.js
+++ src/main/source/static/js/script.js
@@ -1,14 +1,17 @@
 function getFood() {
     console.log("Searching for food");
     // grab input value
-    //var input = $('#cal_input').val();
-    var input = document.querySelector('#cal_input').value;
-    console.log("cal: " + input);
+
+    var input1 = document.querySelector('#cal_input1').value;
+    var input2 = document.querySelector('#cal_input2').value;
+    var input3 = document.querySelector('#cal_input3').value;
+
+    console.log("calories: " + input1 + ", " + input2 + ", " + input3);
   
     // call the search server and get the result
     $.ajax({
-      //url: "http://127.0.0.1:8080/randomize/" + input,
-      url: "http://ec2-3-128-204-114.us-east-2.compute.amazonaws.com:8080/randomize/" + input,
+      //url: "http://127.0.0.1:8080/randomize/" + input1,
+      url: "http://ec2-3-128-204-114.us-east-2.compute.amazonaws.com:8080/randomize/" + input1,
       success: function(res) {
         console.log(res);
   
@@ -21,11 +24,21 @@ function getFood() {
         var food_title = food[0]["title"]
         var food_calories = food[0]["calories"]
 
+        // image link
+        var food_img = "https://spoonacular.com/recipeImages/" + food[0]["id"] + "-" + "556x370" + ".jpg";
+
         console.log(food_title);
         console.log(food_calories);
+        console.log(food_img);
 
-        var row_html_str = "<tr><td>" + food_title + "</td><td>" + food_calories + "</td></tr>"
-        $('#result_table').append(row_html_str);
+        // var row_html_str = "<tr><td>" + food_title + "</td><td>" + food_calories + "</td></tr>"
+        // $('#result_table').append(row_html_str);
+
+        document.getElementById("breakfastMeal").textContent = food_title;
+        document.getElementById("breakfastCal").textContent = food_calories;
+        document.getElementById("breakfastImg").src = food_img;
+        document.getElementById("breakfastImg").style = "height: 200px; width: auto;";
+
         // render the result in the list
   
       },
