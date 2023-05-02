ad574ff04a05cd10d0bbd23abbf27b6f8127222d
18anguyen24 <andrew.nguyen424@gmail.com>
2023-05-01 23:00:44 -0700

    swap key again and add them to script.js

--- src/main/source/static/js/script.js
+++ src/main/source/static/js/script.js
@@ -1,7 +1,9 @@
 const IMG_STYLE = "height: 250px; width: auto; border-radius: 10%;"
 
 async function getFood() {
-    const api_key='8a2108e9f2824129b62849bbf5d70987';
+    //const api_key='8a2108e9f2824129b62849bbf5d70987';
+    const api_key='dcc067f01411450e9f000e5af0f832fc';
+    //const api_key='1860d9cb5fb5432d894efb7ec63f484b';
 
     // grab selected diet pattern
     var dietPattern = document.querySelector('#num_meals_selector').value;
